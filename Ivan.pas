74856db3f70788226dcf7da1310d82e5299fbd4f
Trusty <ivanbordeira2015@hotmail.com>
2025-10-31 20:36:29 -0300

    Estructura de CSVaRegistro

--- Ivan.pas
+++ Ivan.pas
@@ -1,14 +1,14 @@
-Program Example50;
 
-{ Program to demonstrate the Read(Ln) function. }
+Program HellyeahSimulator;
 
+Procedure leerCSV(RelativePath: String);
 Var S : String;
     C : Char;
     F : File of char;
 
 //Leer csv por pantalla caracter por caracter
 begin
-  Assign (F,'SUCURSAL_CENTRO.CSV');
+  Assign (F,'RelativePath');
   Reset (F);
   C:='A';
   Writeln ('The characters before the first space in SUCURSAL_CENTRO.CSV are : ');
@@ -23,4 +23,62 @@ begin
  repeat
    Readln (S);
  until S='';
+end;
+
+
+procedure CSVaRegistro(RelativePath: String);
+var
+FileHandler: File of char;
+aux: String;
+c: char;
+i: Integer;
+Type
+tFecha = Record
+  day: Integer;
+  month: Integer;
+  year: Integer
+end;
+Negocio = Record
+  Seccion: String;
+  Codigo: Integer;
+  Nombre: String;
+  Stock: Integer;
+  Precio: Double;
+  FechaAdq: tFecha;
+  FechaUv: tFecha;
+  FechaCad: tFecha;
+  alta: boolean
+end;
+begin
+  i := 1;
+  c:= 'A';
+  Assign(FileHandler, RelativePath);
+  While not EoF(FileHandler) do
+  begin
+    aux := '';
+    Read(FileHandler,c);
+      While  c <> ',' do
+      aux := aux + c;
+      
+    case i of
+      1: Negocio.Seccion := aux;
+      2: Negocio.Codigo := aux; 
+      3: Negocio.Nombre := aux;
+      4: Negocio.Stock := aux;
+      5: Negocio.Precio := aux;
+      6: Negocio.FechaAdq := aux;
+      7: Negocio.FechaUv := aux;
+      8: Negocio.FechaCad := aux;
+      9: Negocio.alta := aux
+      end;
+      i:= i + 1;
+  end;
+  
+end;
+
+
+
+{ Program to demonstrate the Read(Ln) function. }
+begin
+leerCSV('SUCURSAL_CENTRO.CSV');
 end.

f4ec3d2ebf8bb7a20037ee078a2cd5f6b1431781
Trusty <ivanbordeira2015@hotmail.com>
2025-11-01 16:41:33 -0300

    Implementacion de algoritmo, queda solucionar y factorizar cosas

--- Ivan.pas
+++ Ivan.pas
@@ -1,5 +1,9 @@
 
 Program HellyeahSimulator;
+uses SysUtils;
+
+
+
 
 Procedure leerCSV(RelativePath: String);
 Var S : String;
@@ -8,7 +12,7 @@ Var S : String;
 
 //Leer csv por pantalla caracter por caracter
 begin
-  Assign (F,'RelativePath');
+  Assign (F,RelativePath);
   Reset (F);
   C:='A';
   Writeln ('The characters before the first space in SUCURSAL_CENTRO.CSV are : ');
@@ -25,60 +29,105 @@ begin
  until S='';
 end;
 
-
-procedure CSVaRegistro(RelativePath: String);
-var
-FileHandler: File of char;
-aux: String;
-c: char;
-i: Integer;
 Type
 tFecha = Record
   day: Integer;
   month: Integer;
   year: Integer
 end;
-Negocio = Record
+tRegNegocio = Record
   Seccion: String;
   Codigo: Integer;
   Nombre: String;
   Stock: Integer;
-  Precio: Double;
+  Precio: Integer;
   FechaAdq: tFecha;
   FechaUv: tFecha;
   FechaCad: tFecha;
   alta: boolean
 end;
+
+//Esto da asco también
+procedure cadenAfecha(var Fecha: tFecha; aux: String);
+var
+ i: Integer;
+ c: char;
+ strNumber: String;
+ slash: Integer
+begin
+  strNumber:= '';
+  slash := 0
+  for i := 1 to Length(aux) do
+  begin
+    c:= aux[i];
+    strNumber := strNumber + c;
+    if c = '/' then strNumber := '', slash := slash++;
+    case i of
+    2: Fecha.day := StrToInt(strNumber);
+    5: Fecha.month := StrToInt(strNumber);
+    8: Fecha.year := StrToInt(strNumber)
+    end;
+  end;
+end;
+    
+function cadenAlogico(aux: String):boolean;
+begin
+  cadenAlogico := aux = 'SI';
+end;
+
+
+procedure CSVaRegistro(var Negocio: tRegNegocio; RelativePath: String);
+var
+FileHandler: Text;
+aux: String;
+c: char;
+i: Integer;
+
 begin
   i := 1;
   c:= 'A';
   Assign(FileHandler, RelativePath);
+  Reset(FileHandler);
   While not EoF(FileHandler) do
   begin
+    //Esta seccion es una verga.
     aux := '';
-    Read(FileHandler,c);
-      While  c <> ',' do
-      aux := aux + c;
+    c := 'A';
+      While  (c <> ',') and (not EOLn(FileHandler)) do
+      begin
+      Read(FileHandler,c);
+      aux := aux + c
+      end;
+      if(c = ',') then Delete(aux, Length(aux), 1);
       
+
     case i of
       1: Negocio.Seccion := aux;
-      2: Negocio.Codigo := aux; 
+      2: Negocio.Codigo := StrToInt(aux); 
       3: Negocio.Nombre := aux;
-      4: Negocio.Stock := aux;
-      5: Negocio.Precio := aux;
-      6: Negocio.FechaAdq := aux;
-      7: Negocio.FechaUv := aux;
-      8: Negocio.FechaCad := aux;
-      9: Negocio.alta := aux
+      4: Negocio.Stock := StrToInt(aux);
+      5: Negocio.Precio := StrToInt(aux);
+      6: cadenAfecha(Negocio.FechaAdq, aux);
+      7: cadenAfecha(Negocio.FechaUv, aux);
+      8: cadenAfecha(Negocio.FechaCad, aux);
+      9: Negocio.alta := cadenAlogico(aux);
       end;
+      
       i:= i + 1;
   end;
-  
+  Close(FileHandler)
 end;
 
 
 
+var
+Fecha: tFecha;
+Negocio: tRegNegocio;
 { Program to demonstrate the Read(Ln) function. }
 begin
-leerCSV('SUCURSAL_CENTRO.CSV');
+  //CSVaRegistro(Negocio,'SUCURSAL_CENTRO.CSV');
+  cadenAfecha(Fecha, '02/09/2025');
+  Write(Fecha.year);
+  
+  
 end.

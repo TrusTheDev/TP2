ac639cc91ff8bdfc22f929ab780797aa6118337b
Trusty <ivanbordeira2015@hotmail.com>
2025-11-03 17:31:40 -0300

    CSV a arreglo de registros implementado en ivan.pas

--- Ivan.pas
+++ Ivan.pas
@@ -2,9 +2,6 @@
 Program HellyeahSimulator;
 uses SysUtils;
 
-
-
-
 Procedure leerCSV(RelativePath: String);
 Var S : String;
     C : Char;
@@ -47,87 +44,127 @@ tRegNegocio = Record
   alta: boolean
 end;
 
+ArrRegNegocio = Array [1..500] of tRegNegocio;
+
 //Esto da asco también
 procedure cadenAfecha(var Fecha: tFecha; aux: String);
 var
  i: Integer;
- c: char;
  strNumber: String;
- slash: Integer
+ slash: Integer;
 begin
+  slash := 0;
   strNumber:= '';
-  slash := 0
   for i := 1 to Length(aux) do
   begin
-    c:= aux[i];
-    strNumber := strNumber + c;
-    if c = '/' then strNumber := '', slash := slash++;
-    case i of
-    2: Fecha.day := StrToInt(strNumber);
-    5: Fecha.month := StrToInt(strNumber);
-    8: Fecha.year := StrToInt(strNumber)
+    strNumber := aux[i];
+    if strNumber = '/' then 
+    begin
+      strNumber := '';
+      slash := slash + 1;
+    end; 
+
+    case slash of
+      //En los valores nulos pascal devuelve 0, es valido esto?
+      //Linda ese casting eh
+      0: Fecha.day := StrToInt(IntToStr(Fecha.day) + strNumber);
+      1: Fecha.month := StrToInt(IntToStr(Fecha.month) + strNumber);
+      2: Fecha.year := StrToInt(IntToStr(Fecha.year) + strNumber);
     end;
   end;
 end;
     
 function cadenAlogico(aux: String):boolean;
 begin
-  cadenAlogico := aux = 'SI';
+  cadenAlogico := aux = 'S';
 end;
 
-
-procedure CSVaRegistro(var Negocio: tRegNegocio; RelativePath: String);
+procedure cadenaAregistro(var Negocio: tRegNegocio; cadReg: String);
 var
-FileHandler: Text;
-aux: String;
-c: char;
-i: Integer;
-
+  i: Integer;
+  aux: String;
+  comas: Integer;
 begin
-  i := 1;
-  c:= 'A';
-  Assign(FileHandler, RelativePath);
-  Reset(FileHandler);
-  While not EoF(FileHandler) do
+  aux := '';
+  comas := 0;
+  for i:=1 to Length(cadReg) do
   begin
-    //Esta seccion es una verga.
-    aux := '';
-    c := 'A';
-      While  (c <> ',') and (not EOLn(FileHandler)) do
+    if (cadReg[i] = ',') or (i = Length(cadReg)) then
       begin
-      Read(FileHandler,c);
-      aux := aux + c
-      end;
-      if(c = ',') then Delete(aux, Length(aux), 1);
-      
-
-    case i of
-      1: Negocio.Seccion := aux;
-      2: Negocio.Codigo := StrToInt(aux); 
-      3: Negocio.Nombre := aux;
-      4: Negocio.Stock := StrToInt(aux);
-      5: Negocio.Precio := StrToInt(aux);
-      6: cadenAfecha(Negocio.FechaAdq, aux);
-      7: cadenAfecha(Negocio.FechaUv, aux);
-      8: cadenAfecha(Negocio.FechaCad, aux);
-      9: Negocio.alta := cadenAlogico(aux);
-      end;
-      
-      i:= i + 1;
+        comas := comas + 1;
+        
+        case comas of
+          1: Negocio.Seccion := aux;
+          2: Negocio.Codigo := StrToInt(aux); 
+          3: Negocio.Nombre := aux;
+          4: Negocio.Stock := StrToInt(aux);
+          5: Negocio.Precio := StrToInt(aux);
+          6: cadenAfecha(Negocio.FechaAdq, aux);
+          7: cadenAfecha(Negocio.FechaUv, aux);
+          8: cadenAfecha(Negocio.FechaCad, aux);
+          9: Negocio.alta := cadenAlogico(aux);
+        end;
+        aux := '';
+      end
+    else
+    begin
+      aux := aux + cadReg[i];  
+    end;
   end;
-  Close(FileHandler)
 end;
 
 
+function getFileLinesCount(FilePath: String): Integer;
+var
+  FileText: Text;
+  LineCount: Integer;
+  tempS: String;
+begin
+  Assign(FileText, FilePath);
+  Reset(FileText);
+  LineCount := 0;
+  while not EoF(FileText) do
+  begin
+    ReadLn(FileText, tempS);
+    LineCount := LineCount + 1;
+  end;
+  close(FileText);
+  getFileLinesCount := LineCount;
+end;
+Procedure getFileLine(var tempVar2: String; index: Integer; RelativePath: String);
+Var
+  FileLineHandler: Text;
+  i: Integer;
+Begin
+  Assign(FileLineHandler, RelativePath);
+  Reset( FileLineHandler );
+  tempVar2 := '';
+  //Esta cosa necesita una explicación: https://stackoverflow.com/questions/64556659/how-to-read-a-specific-line-from-a-text-file-in-pascal
+      For i:= 1 To index Do
+      ReadLn(FileLineHandler, tempVar2);
+  Close(FileLineHandler);
+End;
+
+//Faltan agregar restricciones
+procedure CSVaArrRegistro(var Negocios: ArrRegNegocio; dim: Integer;RelativePath: String);
+var
+  linesCount, i: Integer;
+  LineRegister: String;
+  Negocio: tRegNegocio;
+begin
+  linesCount := getFileLinesCount(RelativePath);
+  for i:=1 to linesCount do
+    begin
+      getFileLine(LineRegister,i,RelativePath);
+      dim := dim + 1;
+      cadenaAregistro(Negocio,LineRegister);
+      Negocios[i] := Negocio
+    end;
+end;
+
 
 var
-Fecha: tFecha;
-Negocio: tRegNegocio;
-{ Program to demonstrate the Read(Ln) function. }
+Negocios: ArrRegNegocio;
 begin
-  //CSVaRegistro(Negocio,'SUCURSAL_CENTRO.CSV');
-  cadenAfecha(Fecha, '02/09/2025');
-  Write(Fecha.year);
-  
-  
+  CSVaArrRegistro(Negocios,1,'SUCURSAL_CENTRO.CSV');
 end.
